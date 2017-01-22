ee5e08e216b05ebe35f4632f96980ca8d5097776
gammafp <gammafp@gmail.com>
2017-01-22 14:22:42 +0100

    Inicio de la libreria

---
+++ rsa.js
@@ -0,0 +1,13 @@
+"use strict";
+// Establecer llaves publicas.
+// N = campo compartido
+// E = Calve publicas
+// D = clave privada
+class rsa {
+    constructor(n, e) {
+        [this.n, this.e] = [n, e];
+    }
+
+}
+
+new rsa("Soy a", "Soy b");
